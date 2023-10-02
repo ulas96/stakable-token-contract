354f19108a50a526e69df1d72fba27e3115c8406
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-02 08:04:39 +0300

    framework update

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: process.env.url,
+      accounts: [process.env.privateKey]
+    }
+  }
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+
+
+  const stake = await hre.ethers.deployContract("Stake", [stakable,stk,100000000000,100000000000,1,1]);
+
+  await stake.waitForDeployment();
+
+  console.log(
+   stake.target
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
